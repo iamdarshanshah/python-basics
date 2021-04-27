130b7e9289de826099a3053065ed629d83d5e66e
iamdarshanshah <iamdarshanshah@gmail.com>
2021-04-27 15:59:56 +0530

    added recursions and optional args and keyword args demo

--- functions/functions.py
+++ functions/functions.py
@@ -23,5 +23,60 @@ def calc(a, b):
     z = a / b
     return w, x, y, z
 
+
 calculations = calc(10, 5)
 for i in calculations: print(i)
+
+# Assigning function to a variable
+x = 123
+
+
+def display():
+    x = 678
+    print(x)
+    print(globals()['x'])
+
+
+z = display
+z();
+z();  # same as display()
+display();  # same as z()
+
+
+# Inner Functions (function inside function)
+
+def greet(name):
+    def message():
+        return "Hello "
+
+    greetings = message() + name
+    return greetings
+
+
+print(greet("Darshan"))  # Hello Darshan
+
+
+# print(message()) -> variable message not accessible outside
+
+
+# Function as a parameter to other function
+def greet(func):
+    return "Hello " + func
+
+
+def name(a):
+    return a
+
+
+print(greet(name("Darshan")))  # Hello Darshan
+
+
+# Returning function from function
+def greet():
+    def message():
+        return "Hello"
+    return message
+
+
+fun = greet()
+print(fun())  # Hello
